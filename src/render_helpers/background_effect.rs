8c2a9c556c6428d3b7614628d7cac3a946a2abc5
Ivan Molodetskikh <yalterz@gmail.com>
2026-02-18 21:41:09 +0300

    xray

---
+++ src/render_helpers/background_effect.rs
@@ -0,0 +1,183 @@
+use niri_config::CornerRadius;
+use smithay::backend::renderer::gles::GlesRenderer;
+use smithay::utils::{Logical, Rectangle};
+
+use crate::niri_render_elements;
+use crate::render_helpers::damage::ExtraDamage;
+use crate::render_helpers::xray::{XrayElement, XrayPos};
+use crate::render_helpers::RenderCtx;
+use crate::utils::region::TransformedRegion;
+
+#[derive(Debug)]
+pub struct BackgroundEffect {
+    /// Damage when options change.
+    damage: ExtraDamage,
+    /// Corner radius for clipping.
+    ///
+    /// Stored here in addition to `RenderParams` to damage when it changes.
+    // FIXME: would be good to remove this duplication of radius.
+    corner_radius: CornerRadius,
+    blur_config: niri_config::Blur,
+    options: Options,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Options {
+    pub blur: bool,
+    pub xray: bool,
+    pub noise: Option<f64>,
+    pub saturation: Option<f64>,
+}
+
+impl Options {
+    fn is_visible(&self) -> bool {
+        self.xray
+            || self.blur
+            || self.noise.is_some_and(|x| x > 0.)
+            || self.saturation.is_some_and(|x| x != 1.)
+    }
+}
+
+/// Render-time parameters.
+#[derive(Debug)]
+pub struct RenderParams {
+    /// Geometry of the background effect.
+    pub geometry: Rectangle<f64, Logical>,
+    /// Effect subregion, will be clipped to `geometry`.
+    ///
+    /// `subregion.iter()` should return `geometry`-relative rectangles.
+    pub subregion: Option<TransformedRegion>,
+    /// Geometry and radius for clipping in the same coordinate space as `geometry`.
+    pub clip: Option<(Rectangle<f64, Logical>, CornerRadius)>,
+    /// Scale to use for rounding to physical pixels.
+    pub scale: f64,
+}
+
+impl RenderParams {
+    fn fit_clip_radius(&mut self) {
+        if let Some((geo, radius)) = &mut self.clip {
+            // HACK: increase radius to avoid slight bleed on rounded corners.
+            *radius = radius.expanded_by(1.);
+
+            *radius = radius.fit_to(geo.size.w as f32, geo.size.h as f32);
+        }
+    }
+}
+
+niri_render_elements! {
+    BackgroundEffectElement => {
+        Xray = XrayElement,
+        ExtraDamage = ExtraDamage,
+    }
+}
+
+impl BackgroundEffect {
+    pub fn new(blur_config: niri_config::Blur) -> Self {
+        Self {
+            damage: ExtraDamage::new(),
+            corner_radius: CornerRadius::default(),
+            blur_config,
+            options: Options::default(),
+        }
+    }
+
+    pub fn update_config(&mut self, config: niri_config::Blur) {
+        if self.blur_config == config {
+            return;
+        }
+
+        self.blur_config = config;
+        self.damage.damage_all();
+    }
+
+    pub fn update_render_elements(
+        &mut self,
+        corner_radius: CornerRadius,
+        effect: niri_config::BackgroundEffect,
+        has_blur_region: bool,
+    ) {
+        // If the surface explicitly requests a blur region, default blur to true.
+        let blur = if has_blur_region {
+            effect.blur != Some(false)
+        } else {
+            effect.blur == Some(true)
+        };
+
+        let mut options = Options {
+            blur,
+            xray: effect.xray == Some(true),
+            noise: effect.noise,
+            saturation: effect.saturation,
+        };
+
+        // If we have some background effect but xray wasn't explicitly set, default it to true
+        // since it's cheaper.
+        if options.is_visible() && effect.xray.is_none() {
+            options.xray = true;
+        }
+
+        // FIXME: do we also need to damage when subregion changes? Then we'll need to pass
+        // subregion in update_render_elements().
+        if self.options == options && self.corner_radius == corner_radius {
+            return;
+        }
+
+        self.options = options;
+        self.corner_radius = corner_radius;
+        self.damage.damage_all();
+    }
+
+    pub fn is_visible(&self) -> bool {
+        self.options.is_visible()
+    }
+
+    pub fn render(
+        &self,
+        ctx: RenderCtx<GlesRenderer>,
+        mut params: RenderParams,
+        xray_pos: XrayPos,
+        push: &mut dyn FnMut(BackgroundEffectElement),
+    ) {
+        if !self.is_visible() {
+            return;
+        }
+
+        if let Some(clip) = &mut params.clip {
+            clip.1 = self.corner_radius;
+        }
+        params.fit_clip_radius();
+
+        let damage = self.damage.render(params.geometry);
+
+        // Use noise/saturation from options, falling back to blur defaults if blurred, and
+        // to no effect if not blurred.
+        let blur = self.options.blur && !self.blur_config.off;
+        let noise = if blur { self.blur_config.noise } else { 0. };
+        let noise = self.options.noise.unwrap_or(noise) as f32;
+        let saturation = if blur {
+            self.blur_config.saturation
+        } else {
+            1.
+        };
+        let saturation = self.options.saturation.unwrap_or(saturation) as f32;
+
+        if self.options.xray {
+            let Some(xray) = ctx.xray else {
+                return;
+            };
+
+            push(damage.into());
+            xray.render(
+                ctx,
+                params,
+                xray_pos,
+                blur,
+                noise,
+                saturation,
+                &mut |elem| push(elem.into()),
+            );
+        } else {
+            // Render non-xray effect.
+        }
+    }
+}
